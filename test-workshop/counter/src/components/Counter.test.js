f86e9121787ecafdefa786bf5b19ce79f17783b6
blackggwp <thitikorn.tippawan@thoughtworks.com>
2026-06-04 09:40:05 +0700

    Add tests for Counter component functionality and button interactions

--- test-workshop/counter/src/components/Counter.test.js
+++ test-workshop/counter/src/components/Counter.test.js
@@ -4,22 +4,110 @@ import Counter from "./Counter";
 
 describe("Counter component", () => {
   // TODO: add test for basic render
+  test("should render the component", () => {
+    const props = {
+      value: 0,
+      onIncrement: jest.fn(),
+      onDecrement: jest.fn(),
+    };
+
+    const wrapper = shallow(<Counter {...props} />);
+    expect(wrapper.exists()).toBe(true);
+  });
 
   // TODO: add test for increment button
+  test("should render the increment button", () => {
+    const props = {
+      value: 0,
+      onIncrement: jest.fn(),
+      onDecrement: jest.fn(),
+    };
+
+    const wrapper = shallow(<Counter {...props} />);
+    const buttons = wrapper
+      .find("button")
+      .filterWhere((btn) => btn.text() === "+");
+    expect(buttons.exists()).toBe(true);
+  });
+
   test("first button should call onIncrement", () => {
     const props = {
       value: 2,
       onIncrement: jest.fn(),
-      onDecrement: jest.fn()
+      onDecrement: jest.fn(),
     };
 
     const wrapper = shallow(<Counter {...props} />);
-    const buttons = wrapper.find("button");
-    buttons.at(0).simulate("click");
+    const buttons = wrapper
+      .find("button")
+      .filterWhere((btn) => btn.text() === "+");
+    buttons.simulate("click");
     expect(props.onIncrement).toBeCalled();
   });
 
   // TODO: add test for decrement button
+  test("should render the decrement button", () => {
+    const props = {
+      value: 0,
+      onIncrement: jest.fn(),
+      onDecrement: jest.fn(),
+    };
+
+    const wrapper = shallow(<Counter {...props} />);
+    const buttons = wrapper
+      .find("button")
+      .filterWhere((btn) => btn.text() === "-");
+    expect(buttons.exists()).toBe(true);
+  });
 
   // TODO: add test for third button, third button should call onIncrement when counter is odd
+  test("third button should call onIncrement when counter is odd", () => {
+    const props = {
+      value: 3,
+      onIncrement: jest.fn(),
+      onDecrement: jest.fn(),
+    };
+
+    const wrapper = shallow(<Counter {...props} />);
+    const buttons = wrapper
+      .find("button")
+      .filterWhere((btn) => btn.text() === "Increment if odd");
+    buttons.simulate("click");
+    expect(props.onIncrement).toBeCalled();
+  });
+
+  test("third button should't call onIncrement when counter is even", () => {
+    const props = {
+      value: 2,
+      onIncrement: jest.fn(),
+      onDecrement: jest.fn(),
+    };
+
+    const wrapper = shallow(<Counter {...props} />);
+    const buttons = wrapper
+      .find("button")
+      .filterWhere((btn) => btn.text() === "Increment if odd");
+    buttons.simulate("click");
+    expect(props.onIncrement).not.toBeCalled();
+  });
+
+  // Increment Async button should call onIncrement after 1 second
+  test("Increment Async button should call onIncrement after 1 second", () => {
+    jest.useFakeTimers();
+    const props = {
+      value: 0,
+      onIncrement: jest.fn(),
+      onDecrement: jest.fn(),
+    };
+
+    const wrapper = shallow(<Counter {...props} />);
+    const buttons = wrapper
+      .find("button")
+      .filterWhere((btn) => btn.text() === "Increment async");
+    buttons.simulate("click");
+    expect(props.onIncrement).not.toBeCalled();
+    jest.runAllTimers();
+    expect(props.onIncrement).toBeCalled();
+    jest.useRealTimers();
+  });
 });
