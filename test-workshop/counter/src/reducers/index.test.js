0dfd98af32e7251fbcb8dae9535ecde0a67d5f27
blackggwp <thitikorn.tippawan@thoughtworks.com>
2026-06-04 09:45:47 +0700

    Enable tests for INCREMENT and DECREMENT actions in counter reducer

--- test-workshop/counter/src/reducers/index.test.js
+++ test-workshop/counter/src/reducers/index.test.js
@@ -6,11 +6,13 @@ describe("reducersssss", () => {
       expect(counter(undefined, {})).toBe(0);
     });
 
-    test.skip("should handle INCREMENT action", () => {
+    test("should handle INCREMENT action", () => {
       expect(counter(1, { type: "INCREMENT" })).toBe(2);
     });
 
-    // TODO: add test for "DECREMENT" action
+    test("should handle DECREMENT action", () => {
+      expect(counter(1, { type: "DECREMENT" })).toBe(0);
+    });
 
     test("should ignore unknown actions", () => {
       expect(counter(1, { type: "unknown" })).toBe(1);
